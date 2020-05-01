3cb403018eceb8035773c61a5af95a3ce18db879
Yoonhee <yoonhee.jo.cs@gmail.com>
2020-04-30 22:26:56 -0400

    get contacts from server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {ContactsComponent} from './contacts/contacts.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/service/contact.ts
@@ -0,0 +1,8 @@
+export interface Contact {
+  id: string;
+  user: string;
+  name: string;
+  openingDate: string;
+  expirationDate: string;
+  contactType: string;
+}
