027883f05b90e434cdca80e97d61b46b5e1ad325
Yoonhee <yoonhee.jo.cs@gmail.com>
2025-03-21 15:48:29 -0400

    Update: Replace ContactMe.jpg with contact_lenses_homepage.jpg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  imgPath = '../assets/ContactMe.jpg';
+  imgPath = '../assets/contact_lenses_homepage.jpg';
   alt = 'Contact me logo';
 
   constructor() {
